fcbfa2e2d9e986bcdc853641f0670ddbe05cca0e
Serap <77828715+serapo@users.noreply.github.com>
2022-06-06 16:42:56 +0300

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+const btnyedi=document.getElementById("yedi");
+const sekiz=document.getElementById("sekiz");
+const esittir=document.getElementById("esittir");
+const sonuc=document.querySelector("#sonuc");
+
+
+sekiz.addEventListener('click',()=>{
+    sonuc.innerHTML=`<h1>8</h1>`;
+});
