9f208499eb901bef17dcf876616effc7c205ce13
Serap <77828715+serapo@users.noreply.github.com>
2022-06-07 10:54:10 +0300

    js

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ const bes=document.getElementById("bes");
 const altı=document.getElementById("altı");
 const yedi=document.getElementById("yedi");
 const sekiz=document.getElementById("sekiz");
+const dokuz=document.getElementById("dokuz");
 
 const sonuc=document.getElementById("sonuc");
 
-const dokuz=document.getElementById("dokuz");
+const eksi=document.getElementById("eksi");
+const arti=document.getElementById("arti");
+const artıeksi=document.getElementById("artıeksi");
+const carpı=document.getElementById("carpı");
+const esittir=document.getElementById("esittir");
+const bolum=document.getElementById("bolum");
+const yuzde=document.getElementById("yüzde");
+
 const sifir=document.getElementById("sifir");
+const nokta=document.getElementById("nokta");
 const AC=document.getElementById("AC");
 
 bir.addEventListener('click',()=>{sayiyaz(1);})
@@ -23,6 +32,13 @@ yedi.addEventListener('click',() =>{sayiyaz(7);});
 sekiz.addEventListener('click',() =>{sayiyaz(8);});
 dokuz.addEventListener('click',() =>{sayiyaz(9);});
 sifir.addEventListener('click',() =>{sayiyaz(0);});
+
+arti.addEventListener('click',() =>{sayiyaz("+");});
+carpı.addEventListener('click',() =>{sayiyaz("x");});
+bolum.addEventListener('click',() =>{sayiyaz("÷");});
+yuzde.addEventListener('click',() =>{sayiyaz("");});
+esittir.addEventListener('click',() =>{hesapla();});
+eksi.addEventListener('click',() =>{sayiyaz("-");});
 AC.addEventListener('click', () =>{ sonuc.innerHTML=`0`});
 
 function sayiyaz(rakam){ 
@@ -32,3 +48,48 @@ function sayiyaz(rakam){
      }
      sonuc.innerHTML +=rakam;
 }
+
+function hesapla(){
+     if(!sonuc.innerHTML){
+         alert("Please enter a number");
+     }
+     else if(sonuc.innerHTML.includes("+")){
+         let deger=sonuc.innerHTML.split("+");
+         sonuc.innerHTML=Number(deger[0])+Number(deger[1]);
+     }
+     else if(sonuc.innerHTML.includes("-")){
+        let deger=sonuc.innerHTML.split("-");
+        sonuc.innerHTML=Number(deger[0])-Number(deger[1]);
+    }
+    else if(sonuc.innerHTML.includes("x") ){
+        let deger=sonuc.innerHTML.split("x");
+        sonuc.innerHTML=(Number(deger[0])*Number(deger[1]));
+    } 
+    else if(sonuc.innerHTML.includes("÷") ){
+        let deger=sonuc.innerHTML.split("÷");
+        sonuc.innerHTML=(Number(deger[0])/Number(deger[1]));
+    }
+}
+
+yuzde.addEventListener('click',() =>{let yuzde=Number(sonuc.innerHTML); sonuc.innerHTML=(yuzde/100).toFixed(2);});
+artıeksi.addEventListener('click',() =>{
+    if(sonuc.innerHTML=="0"){
+        sonuc.innerHTML=`0`;
+    }
+   else if(!sonuc.innerHTML.includes("-"))
+   sonuc.innerHTML="-" + `${sonuc.innerHTML}`;
+   else if(sonuc.innerHTML.includes('-')){
+       let yazi=sonuc.innerHTML.substring(1);
+    sonuc.innerHTML=`${yazi}`;
+   }
+});
+
+nokta.addEventListener('click',() =>{
+    if(!sonuc.innerHTML.includes(".")){
+        sayiyaz(".");
+    }
+    else if(sonuc.innerHTML.includes(".")){
+        let yazi=sonuc.innerHTML;
+        sonuc.innerHTML=`${yazi}`;
+    }
+});
